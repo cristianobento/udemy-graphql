eda3100b61c34811c8d5d9389c46456374f487a6
Cristiano Oliveira <cristiano.bento@kroton.com.br>
2019-07-22 10:20:54 -0300

    feat: Adding input for user update

--- 003-mutation/resolvers/Mutation.js
+++ 003-mutation/resolvers/Mutation.js
@@ -1,5 +1,19 @@
 const { usuarios, proximoId } = require("../data/db");
 
+function indiceUsuario(filtro) {
+  if (!filtro) return -1;
+
+  const { id, email } = filtro;
+
+  if (id) {
+    return usuarios.findIndex(u => u.id === id);
+  } else if (email) {
+    return usuarios.findIndex(u => u.email === email);
+  }
+
+  return -1;
+}
+
 module.exports = {
   novoUsuario(_, { dados }) {
     const emailExistente = usuarios.some(u => u.email === dados.email);
@@ -19,26 +33,33 @@ module.exports = {
     return novo;
   },
 
-  excluirUsuario(_, { id }) {
-    const i = usuarios.findIndex(u => u.id === id);
+  excluirUsuario(_, { filtro }) {
+    const i = indiceUsuario(filtro);
 
     if (i < 0) return null;
     const excluidos = usuarios.splice(i, 1);
     return excluidos ? excluidos[0] : null;
   },
 
-  alterarUsuario(_, args) {
-    const i = usuarios.findIndex(u => u.id === args.id);
+  alterarUsuario(_, { filtro, dados }) {
+    const i = indiceUsuario(filtro);
 
     if (i < 0) return null;
 
-    const usuario = {
-      ...usuarios[i],
-      ...args
-    };
+    // const usuario = {
+    //   ...usuarios[i],
+    //   ...args
+    // };
 
-    usuarios.splice(i, 1, usuario);
+    usuarios[i].nome = dados.nome;
+    usuarios[i].email = dados.email;
+    if (usuarios[i].idade) {
+      usuarios[i].idade = dados.idade;
+    }
 
-    return usuario;
+    // usuarios.splice(i, 1, usuario);
+
+    // return usuario;
+    return usuarios[i];
   }
 };
