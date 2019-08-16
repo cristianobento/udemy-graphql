ebe77924980fd7e815bbea82da9f20025c1ffcfd
Cristiano Oliveira <cristiano.bento@kroton.com.br>
2019-08-16 02:19:52 -0300

    feat: Adding profile migrations

--- 004-database/knexfile.js
+++ 004-database/knexfile.js
@@ -1,9 +1,14 @@
 // Update with your config settings.
-const connection = require("./.env");
 
 module.exports = {
   client: "mysql",
-  connection,
+  connection: {
+    host: "172.19.0.16",
+    port: "3306",
+    database: "exercises",
+    user: "root",
+    password: "root"
+  },
   pool: {
     min: 2,
     max: 10
--- 004-database/migrations/20190816011659_tabela_perfis.js
+++ 004-database/migrations/20190816011659_tabela_perfis.js
@@ -1,8 +1,14 @@
-
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("perfis", table => {
+    table.increments("id").primary();
+    table
+      .string("nome")
+      .notNull()
+      .unique();
+    table.string("rotulo").notNull();
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("perfis");
 };
