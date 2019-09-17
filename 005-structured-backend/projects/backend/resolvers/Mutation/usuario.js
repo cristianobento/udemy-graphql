eeb20f658ced61c2242d9a183aa137b5c8b4f7bb
Cristiano Oliveira <cristiano.bento@kroton.com.br>
2019-09-17 09:28:40 -0300

    feat: Adding new mutation

--- 005-structured-backend/projects/backend/resolvers/Mutation/usuario.js
+++ 005-structured-backend/projects/backend/resolvers/Mutation/usuario.js
@@ -1,19 +1,39 @@
+const bcrypt = require("bcrypt-nodejs");
 const db = require("../../config/db");
 const { perfil: obterPerfil } = require("../Query/perfil");
 const { usuario: obterUsuario } = require("../Query/usuario");
 
-module.exports = {
+const mutations = {
+  registrarUsuario(_, { dados }) {
+    return mutations.novoUsuario(_, {
+      dados: {
+        nome: dados.nome,
+        email: dados.email,
+        senha: dados.senha
+      }
+    });
+  },
   async novoUsuario(_, { dados }) {
     try {
       const idsPerfis = [];
-      if (dados.perfis) {
-        for (let filtro of dados.perfis) {
-          const perfil = await obterPerfil(_, {
-            filtro
-          });
-          if (perfil) idsPerfis.push(perfil.id);
-        }
+
+      if (!dados.perfis || !dados.perfis.length) {
+        dados.perfis = [
+          {
+            nome: "comum"
+          }
+        ];
       }
+
+      for (let filtro of dados.perfis) {
+        const perfil = await obterPerfil(_, {
+          filtro
+        });
+        if (perfil) idsPerfis.push(perfil.id);
+      }
+
+      const salt = bcrypt.genSaltSync();
+      dados.senha = bcrypt.hashSync(dados.senha, salt);
 
       delete dados.perfis;
       const [id] = await db("usuarios").insert(dados);
@@ -70,6 +90,9 @@ module.exports = {
           }
         }
 
+        const salt = bcrypt.genSaltSync();
+        dados.senha = bcrypt.hashSync(dados.senha, salt);
+
         delete dados.perfis;
         await db("usuarios")
           .where({ id })
@@ -81,3 +104,5 @@ module.exports = {
     }
   }
 };
+
+module.exports = mutations;
